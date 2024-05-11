ae8e65d956723114c69b380257a644d46ad2f2f2
fcongedo <franco_congedo@hotmail.com>
2024-05-11 12:41:01 -0300

    update controllers

--- src/main/java/com/example/peliculas/controllers/PeliculasController.java
+++ src/main/java/com/example/peliculas/controllers/PeliculasController.java
@@ -1,5 +1,34 @@
 package com.example.peliculas.controllers;
 
-public class PeliculasController {
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
+import com.example.peliculas.entities.Pelicula;
+import com.example.peliculas.services.IPeliculaService;
+
+@Controller
+public class PeliculasController {
+	private IPeliculaService service;
+
+	public PeliculasController(IPeliculaService service) {
+		this.service = service;
+
+	}
+
+	@GetMapping("/pelicula")
+	public String crear(Model model) {
+		Pelicula pelicula = new Pelicula();
+		model.addAttribute("pelicula", pelicula);
+		model.addAttribute("titulo", "Nueva Pelicula");
+		return "pelicula";
+	}
+	@GetMapping("/pelicula/{id}")
+	public String crear(@PathVariable(name="id") Long id, Model model) {
+		Pelicula pelicula = new Pelicula();
+		model.addAttribute("pelicula", pelicula);
+		model.addAttribute("titulo", "Nueva Pelicula");
+		return "pelicula";
+	}
 }
