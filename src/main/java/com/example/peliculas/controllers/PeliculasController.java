be3045323a3f1a74f5dc98c59346c83762df4909
fcongedo <franco_congedo@hotmail.com>
2024-05-11 13:20:44 -0300

    Update controller

--- src/main/java/com/example/peliculas/controllers/PeliculasController.java
+++ src/main/java/com/example/peliculas/controllers/PeliculasController.java
@@ -4,16 +4,20 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import com.example.peliculas.entities.Pelicula;
+import com.example.peliculas.services.IGeneroService;
 import com.example.peliculas.services.IPeliculaService;
 
 @Controller
 public class PeliculasController {
 	private IPeliculaService service;
+	private IGeneroService generoService;
 
-	public PeliculasController(IPeliculaService service) {
+	public PeliculasController(IPeliculaService service, IGeneroService generoService) {
 		this.service = service;
+		this.generoService = generoService;
 
 	}
 
@@ -21,6 +25,7 @@ public class PeliculasController {
 	public String crear(Model model) {
 		Pelicula pelicula = new Pelicula();
 		model.addAttribute("pelicula", pelicula);
+		model.addAttribute("generos", generoService.findAll());
 		model.addAttribute("titulo", "Nueva Pelicula");
 		return "pelicula";
 	}
@@ -30,5 +35,13 @@ public class PeliculasController {
 		model.addAttribute("pelicula", pelicula);
 		model.addAttribute("titulo", "Nueva Pelicula");
 		return "pelicula";
+	}
+	@PostMapping("/pelicula")
+	public String guardar(Pelicula pelicula) {
+		return "redirect:home";
+	}
+	@GetMapping({"/","/home","/index"})
+	public String home() {
+		return "home";
 	}
 }
